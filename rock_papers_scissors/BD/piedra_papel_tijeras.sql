36455a3b6503725297218077f1528f44c4309485
arjona55 <alexarjona77@gmail.com>
2024-03-22 17:18:30 +0100

    final_final

--- rock_papers_scissors/BD/piedra_papel_tijeras.sql
+++ rock_papers_scissors/BD/piedra_papel_tijeras.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-03-2024 a las 17:41:30
+-- Tiempo de generación: 22-03-2024 a las 17:16:59
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -24,32 +24,13 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `partidas`
---
-
-CREATE TABLE `partidas` (
-  `ID_Partida` int(11) NOT NULL,
-  `Usuario` varchar(30) NOT NULL,
-  `Resultado` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `partidas`
---
-
-INSERT INTO `partidas` (`ID_Partida`, `Usuario`, `Resultado`) VALUES
-(1, 'Nick_Ejemplo', 'Ganador');
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `usuarios`
 --
 
 CREATE TABLE `usuarios` (
   `Nick` varchar(30) NOT NULL,
   `Mail` varchar(50) NOT NULL,
-  `Contraseña` varchar(30) NOT NULL
+  `Contraseña` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -57,18 +38,14 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`Nick`, `Mail`, `Contraseña`) VALUES
-('Nick_Ejemplo', 'ejemplo@ejemplo.com', '1234');
+('Nick_Ejemplo', 'ejemplo@ejemplo.com', '1234'),
+('prueba1', 'prueba1@prueba.com', 'pbkdf2:sha256:260000$L4JeW3FGzU8c1iNh$d6599504b0c2243b5ddaa9998a638ff403cc30283e7d69e3a0f1fb93c50ad5b9'),
+('prueba2', 'prueba2@prueba.com', 'pbkdf2:sha256:260000$KsjysEPlB9FsVob7$1457958731f7b89219ff887cfdb6f06602304de68c42c3b94ff94f5872fe0438'),
+('prueba3', 'prueba3@prueba.com', 'pbkdf2:sha256:260000$6wm2X3hMHJ9F9lBq$f82881beb2977461ab9d2fa934661395f31ab40918bacb96febafec548a5f0a8');
 
 --
 -- Índices para tablas volcadas
 --
-
---
--- Indices de la tabla `partidas`
---
-ALTER TABLE `partidas`
-  ADD PRIMARY KEY (`ID_Partida`),
-  ADD KEY `Usuario` (`Usuario`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -77,26 +54,6 @@ ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`Nick`),
   ADD UNIQUE KEY `Mail` (`Mail`),
   ADD KEY `Mail_2` (`Mail`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `partidas`
---
-ALTER TABLE `partidas`
-  MODIFY `ID_Partida` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `partidas`
---
-ALTER TABLE `partidas`
-  ADD CONSTRAINT `partidas_ibfk_1` FOREIGN KEY (`Usuario`) REFERENCES `usuarios` (`Nick`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
